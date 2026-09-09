fda272b0eb62dec048ae27cd6e9c5fae15321247
28ashapiro <28ashapiro@live.ytech.edu>
2026-09-09 13:36:20 -0400

    pogress

---
+++ docpac_041109/Unit1Lesson4/ShapiroAiden/server.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv')
+const readline = require('node:readline');
+const http = require('http')
+const fs = require('fs')
+const server = http.createServer((req, res) => {
+    const method = req.method
+    const url = req.url
+    console.log(`recieved ${method} to ${url}`)
+    if (req.url == '/pages/form.html') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile('pages/form.html', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            console.log()
+            res.end(data);
+        });
+    } else {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write('<h1>local server!</h1>')
+    res.write('This is a Node.js application running on the local host, and listening to a set port. (loaclhost:3000)')
+    res.end();
+    }
+});
+dotenv.config();
+const PORT = process.env.PORT
+server.listen(PORT, 'localhost', () => {
+    console.log(`server running at http://localhost:${PORT}/`)
+});
