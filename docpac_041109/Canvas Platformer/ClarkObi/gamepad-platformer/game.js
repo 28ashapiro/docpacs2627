7fc03f7bfa46ca4f911376dbcdd28a2e470d23e8
Obi174 <28OClark@live.ytech.edu>
2026-09-20 20:30:25 -0400

    Updating Canvas platformer

--- docpac_041109/Canvas Platformer/ClarkObi/gamepad-platformer/game.js
+++ docpac_041109/Canvas Platformer/ClarkObi/gamepad-platformer/game.js
@@ -1,3 +1,30 @@
 let canvas = document.getElementById("gameCanvas")
-let ctx = getContext("2d")
-fillRect(50,50,100,100)
+let ctx = canvas.getContext("2d")
+
+class wall {
+    constructor(x,y,w,h){
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;}
+    draw(){ctx.fillRect(
+        this.x,
+        this.y,
+        this.w,
+        this.h
+    )}
+}
+class player extends wall{
+    constructor(x,y,w,h){super(x,y,w,h)    
+    this.vx=0;this.vy=0;this.speed=5;this.jumpStrength=10;this.grounded=false}
+    draw(){ctx.fillRect('yellow')}
+}
+gameRunning=true
+let backWall = new wall(25,25,600,300);
+function setup() {
+    backWall.draw();
+}
+function loop() {
+}
+function end() {
+}
